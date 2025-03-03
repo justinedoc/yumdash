bb9bf8f9d7db3f2a5ebe087443a3620ff42ca0d7
Onyiriuka Justin <richardjustin500@gmail.com>
2025-03-03 22:15:22 +0100

    customer pages added

---
+++ src/components/landing/customer/index.tsx
@@ -0,0 +1,25 @@
+import DownloadApp from "./_components/DownloadApp";
+import EarnWithUs from "./_components/EarnWithUs";
+import Faq from "./_components/Faq";
+import Footer from "./_components/Footer";
+import Hero from "./_components/Hero";
+import HeroCallToAction from "./_components/HeroCallToAction";
+import NewsLetter from "./_components/NewsLetter";
+import Subscribe from "./_components/Subscribe";
+
+function Customer() {
+  return (
+    <section className="w-full overflow-x-hidden">
+      <Hero />
+      <HeroCallToAction />
+      <DownloadApp />
+      <EarnWithUs />
+      <Subscribe />
+      <Faq />
+      <NewsLetter />
+      <Footer />
+    </section>
+  );
+}
+
+export default Customer;
