52db55482104c38572181b5f0e5e77d46df56cdc
Onyiriuka Justin <richardjustin500@gmail.com>
2025-03-06 20:11:35 +0100

    animate load component added for auth routes

---
+++ src/components/auth/customer/_components/AnimateLoad.tsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+import Loading from "../../ui/Loading";
+
+export const LoadingContext = createContext<
+  | {
+      isLoading: boolean;
+      handleLoading: (event: "start" | "end") => void;
+    }
+  | undefined
+>(undefined);
+
+export function AnimateLoad({ children }: { children: React.ReactNode }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLoading = (event: "start" | "end") => {
+    setIsLoading(event === "start");
+  };
+  return (
+    <LoadingContext.Provider value={{ isLoading, handleLoading }}>
+      {isLoading && <Loading />}
+      {children}
+    </LoadingContext.Provider>
+  );
+}
